a7bad06d2ed4de6171696aa9839758a29e194872
CIKADA <adel.garipovazn@mail.ru>
2023-07-15 01:21:00 +0300

    Create AppBadge

---
+++ components/badge/AppBadge.vue
@@ -0,0 +1,21 @@
+<template>
+  <div 
+    class="badge inline-block rounded-full px-3 py-2"
+    :class="[bgColor, textColor]"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    bgColor: {
+      type: String,
+      required: true,
+    },
+    textColor: {
+      type: String,
+      default: 'text-slate-50'
+    }
+  });
+</script>
