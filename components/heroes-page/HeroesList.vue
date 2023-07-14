d89b14afede5c363c42965cd5c2ad7640bac758a
CIKADA <adel.garipovazn@mail.ru>
2023-07-15 01:20:09 +0300

    Create HeroesList

---
+++ components/heroes-page/HeroesList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="flex flex-col gap-4">
+    <HeroItem 
+      v-for="(hero, idx) in items" :key="idx"
+      :item="hero"
+    />
+  </ul>
+</template>
+
+<script setup>
+import HeroItem from '@/components/heroes-page/HeroItem.vue'
+
+  const props = defineProps({
+    items: {
+      type: Array,
+      required: true,
+    },
+  });
+</script>
