3214c17a4efccd9732f60fe24e3940a42b0e4d95
CIKADA <adel.garipovazn@mail.ru>
2023-07-15 01:19:18 +0300

    Change default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,6 @@
 <template>
-  <div>
-    <header class="shadow-sm bg-white">
-      <nav class="container mx-auto p-4 flex justify-between">
-        <NuxtLink class="font-bold" to="/">Rick & Morty</NuxtLink>
-        <ul class="flex items-center gap-4">
-          <li>
-            <NuxtLink to="/">Main</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink class="btn" to="/about">About me</NuxtLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-
+  <div class="bg-gray-200 min-w-[330px]">
+    <TheHeader />
     <div class="container mx-auto p-4">
       <slot />
     </div>
@@ -21,7 +8,6 @@
 </template>
 
 <script setup lang="ts">
+import TheHeader from '@/components/header/TheHeader.vue'
 
 </script>
-
-<style scoped></style>
