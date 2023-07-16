84a595c51647d139d696e8154c4628b1629a20e7
CIKADA <adel.garipovazn@mail.ru>
2023-07-16 18:56:01 +0300

    Change main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,72 @@
 <template>
-  <div class="grid grid-cols-4 gap-5">
-    <div v-for="(p, idx) in heroes" :key="idx">
-      <NuxtLink to="/">{{ p }}</NuxtLink>
+  <section class="hero">
+    <div class="hero__row flex flex-col gap-4">
+      <div class="hero__column flex justify-left md:justify-end items-end flex-wrap gap-4">
+        <AppInput 
+          v-model:value="heroName"
+          name="heroName"
+          label="Поиск героя по имени:"
+          placeholder="Введите имя героя"
+          width="300px"
+        />
+        <AppSelect
+          :options="heroStatuses"
+          v-model:value="heroStatus"
+          width="300px"
+        />
+      </div>
+      <div class="hero__column">
+        <HeroesList 
+          :items="heroesStore.getHeroes"
+        />
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup>
-  const response = await useFetch('https://rickandmortyapi.com/api/character')
-  const heroes = await response.data.value.results
+import AppInput from '@/components/input/AppInput.vue';
+import AppSelect from '@/components/select/AppSelect.vue';
+import HeroesList from '@/components/heroes-page/HeroesList.vue'
+import {useHeroesStore} from '@/store/heroes'
+
+  const heroesStore = useHeroesStore();
+
+  onServerPrefetch(async () => {
+    await heroesStore.fetchHeroes()
+  });
+
+  
+  const heroName = ref('');
+  const heroStatus = ref('Выбрать');
+
+  const heroStatuses = ref([
+    { name: 'Жив', value: 'alive' },
+    { name: 'Погиб', value: 'dead' },
+    { name: 'Неизвестно', value: 'unknown' },
+  ]);
+
+  const heroStatusConver = (statusText) => {
+    switch (statusText) {
+      case 'Жив':
+        return 'alive'
+        break;
+      case 'Погиб':
+        return 'dead'
+        break;
+      case 'Неизвестно':
+        return 'unknown'
+        break;
+      default:
+        return null
+        break;
+    }
+  }
+
+  watch([heroName, heroStatus], ([heroName, heroStatus]) => {
+    heroesStore.filterHeroes({
+      name: heroName,
+      status: heroStatusConver(heroStatus)
+    });
+  });
 </script>
-
-<style scoped>
-
-</style>
