da85cb6be4ec4444eaaea4b4a37e28bb89ecb088
CIKADA <adel.garipovazn@mail.ru>
2023-07-16 18:54:52 +0300

    Add heroes fetch methods

--- store/heroes.js
+++ store/heroes.js
@@ -1,17 +1,69 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
 export const useHeroesStore = defineStore("heroes", {
   state: () => {
     return {
-      heroes: [
-        {
-          id: 1,
-          name: "Джесси Пинкман",
-        },
-      ],
+      heroes: [],
+      currentHero: {},
     };
   },
-  actions: {},
-  getters: {},
+  getters: {
+    getHeroes() {
+      return this.heroes;
+    },
+    getCurrentHeroe() {
+      return this.currentHero;
+    },
+  },
+  actions: {
+    async fetchHeroes() {
+      try {
+        const response = await axios.get(
+          "https://rickandmortyapi.com/api/character"
+        );
+        const heroes = await response.data;
+        const data = await heroes;
+        this.setHeroes(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async filterHeroes({ name, status }) {
+      try {
+        const params = new URLSearchParams();
+        if (name) {
+          params.append("name", name);
+        }
+        if (status) {
+          params.append("status", status);
+        }
+
+        const response = await axios.get(
+          `https://rickandmortyapi.com/api/character/?${params.toString()}`
+        );
+        const heroes = await response.data;
+        const data = await heroes;
+        this.setHeroes(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async fetchCurrentHeroe(characterID) {
+      try {
+        const response = await axios.get(
+          `https://rickandmortyapi.com/api/character/${characterID}`
+        );
+        const heroe = await response.data;
+        const data = await heroe;
+        this.currentHero = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    setHeroes(payload) {
+      this.heroes = payload;
+    },
+  },
   persist: true,
 });
